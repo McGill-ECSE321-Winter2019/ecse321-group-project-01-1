77929c4579afc8d198f6c93b95d0d6eec65e3b6b
Tri-tin Truong <mark.zhu@mail.mcgill.ca>
2019-02-10 17:34:11 +0900

    Started .dao and did initial imports. No methods yet

---
+++ Backend/Backend/src/main/java/ca/mcgill/ecse321/BackendApplication/dao/BackendApplicationRepository.java
@@ -0,0 +1,23 @@
+package ca.mcgill.ecse321.BackendApplication.dao;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.backend.model.AcademicSemester;
+import ca.mcgill.ecse321.backend.model.ApplicationForm;
+import ca.mcgill.ecse321.backend.model.Document;
+import ca.mcgill.ecse321.backend.model.DocumentType;
+import ca.mcgill.ecse321.backend.model.Reminder;
+import ca.mcgill.ecse321.backend.model.Student;
+
+public class BackendApplicationRepository {
+
+}
