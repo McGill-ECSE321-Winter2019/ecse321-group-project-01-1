eb99d94505e11cf33132dea41230c096382fb0ae
Binyuan Sun <sunbinyuan01@hotmail.com>
2019-03-03 23:55:31 -0500

    User (#46)
    * Adding testing tools
    
    * Added test and fixed student service (weird issue with manually set @Id)
    
    * Remove PostgreSQL config

--- Backend/Backend/src/main/java/ca/mcgill/ecse321/backend/service/StudentService.java
+++ Backend/Backend/src/main/java/ca/mcgill/ecse321/backend/service/StudentService.java
@@ -1,11 +1,21 @@
 package ca.mcgill.ecse321.backend.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
+import javax.validation.Validator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import ca.mcgill.ecse321.backend.dao.StudentRepository;
@@ -13,16 +23,21 @@ import ca.mcgill.ecse321.backend.dto.StudentDto;
 import ca.mcgill.ecse321.backend.model.Student;
 
 @Service
+@Validated
 public class StudentService {
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	@Autowired
 	StudentRepository studentRepository;
 	
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-
+	
 	@Transactional
 	public Student createStudent(@ModelAttribute("student") @Valid StudentDto studentDto) throws Exception {
-        if (emailExists(studentDto.getEmail())) {   
+		
+		if (emailExists(studentDto.getEmail())) {   
             throw new Exception(
               "There is already a student with that email address: " + studentDto.getEmail());
         }
@@ -33,8 +48,24 @@ public class StudentService {
 		S.setLastName(studentDto.getLastName());
 		S.setEmail(studentDto.getEmail());
 		S.setPassword(passwordEncoder.encode(studentDto.getPassword()));
-		S = studentRepository.save(S);
+		entityManager.persist(S);
+
 		return S;
+	}
+	
+	@Transactional
+	public Student findStudentByStudentID(String studentID) {
+		return studentRepository.findStudentByStudentID(studentID);
+	}
+	
+	@Transactional
+	public Student findStudentByEmail(String email) {
+		return studentRepository.findStudentByEmail(email);
+	}
+	
+	@Transactional
+	public Student findStudentById(String studentID) {
+		return studentRepository.findStudentByStudentID(studentID);
 	}
 	
     private boolean emailExists(String email) {
@@ -44,6 +75,11 @@ public class StudentService {
         }
         return false;
     }
+    
+	@Transactional
+	public List<Student> getAll() {
+		return toList(studentRepository.findAll());
+	}
 	
     public StudentDto toDto(Student student) {
 		StudentDto studentDto = new StudentDto();
@@ -55,6 +91,14 @@ public class StudentService {
 		return studentDto;
     	
     }
+    
+	private <T> List<T> toList(Iterable<T> iterable){
+		List<T> resultList = new ArrayList<T>();
+		for (T t : iterable) {
+			resultList.add(t);
+		}
+		return resultList;
+	}
     
 }
 
