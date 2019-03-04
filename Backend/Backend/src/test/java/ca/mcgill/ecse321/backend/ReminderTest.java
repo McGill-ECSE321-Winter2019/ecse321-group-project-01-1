3914d5dcd5d4ce08f98942bfaafd2728ca88d2f6
Tri-tin Truong <mark.zhu@mail.mcgill.ca>
2019-03-04 13:30:38 +0900

    Started remindertest

---
+++ Backend/Backend/src/test/java/ca/mcgill/ecse321/backend/ReminderTest.java
@@ -0,0 +1,31 @@
+package ca.mcgill.ecse321.backend;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Set;
+
+import java.util.*;
+
+import javax.transaction.Transactional;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import ca.mcgill.ecse321.backend.model.*;
+import ca.mcgill.ecse321.backend.dao.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReminderTest {
+
+}
