4f5eb84b8da825d0797870fcbbe317ab62fe8f17
QuintusHe <zirui.he@mail.mcgill.ca>
2019-03-06 03:26:55 -0500

    Merge branch 'dev' into testing

--- Backend/Backend/src/main/java/ca/mcgill/ecse321/backend/controller/ProfileViewingController.java
+++ Backend/Backend/src/main/java/ca/mcgill/ecse321/backend/controller/ProfileViewingController.java
@@ -2,6 +2,7 @@ package ca.mcgill.ecse321.backend.controller;
 
 import ca.mcgill.ecse321.backend.model.Internship;
 import ca.mcgill.ecse321.backend.model.Student;
+import ca.mcgill.ecse321.backend.service.AuthenticationService;
 import ca.mcgill.ecse321.backend.service.InternshipService;
 import ca.mcgill.ecse321.backend.service.StudentService;
 import ca.mcgill.ecse321.backend.dto.*;
@@ -20,10 +21,15 @@ public class ProfileViewingController {
     @Autowired
     private StudentService studentService;
 
+    @Autowired
+    private AuthenticationService authenticationService;
 
     @GetMapping(value = {"/api/profile", "/api/profile/"})
-    public StudentDto getStudentProfile(@RequestParam("student") Student student){
-        return studentService.toDto(student);
+    public StudentDto getStudentProfile(){
+    	Student student = authenticationService.getCurrentStudent();
+        StudentDto studentDto = studentService.toDto(student);
+        studentDto.setInternship(getInternshipDtos(student));
+        return studentDto;
     }
 
     public Set<InternshipDto> getInternshipDtos(Student student){
