e0ea5aaf0c761f5dbfa63d81b19d0a680eaca288
Binyuan Sun <sunbinyuan01@hotmail.com>
2019-03-22 02:20:28 -0400

    Added Login component and CORS configuration for Spring Security

--- Backend/Backend/src/main/java/ca/mcgill/ecse321/backend/controller/ApplicationFormController.java
+++ Backend/Backend/src/main/java/ca/mcgill/ecse321/backend/controller/ApplicationFormController.java
@@ -1,18 +1,26 @@
 package ca.mcgill.ecse321.backend.controller;
 
+import java.sql.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import ca.mcgill.ecse321.backend.dto.ApplicationFormDto;
 import ca.mcgill.ecse321.backend.model.ApplicationForm;
 import ca.mcgill.ecse321.backend.model.Internship;
 import ca.mcgill.ecse321.backend.service.ApplicationFormService;
 import ca.mcgill.ecse321.backend.service.AuthenticationService;
 import ca.mcgill.ecse321.backend.service.InternshipService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.web.bind.annotation.*;
-
-import java.sql.Date;
 
 @RestController
+@CrossOrigin(origins = "*")
+
 public class ApplicationFormController {
     
     @Autowired
--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Login from '@/components/Login'
+
+
 
 Vue.use(Router)
 
@@ -10,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
