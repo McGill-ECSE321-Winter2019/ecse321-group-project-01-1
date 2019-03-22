5d1bbdbebde62198c1956ba19dfa8058d4c18051
Tritin Truong <tritin_truong@yahoo.com>
2019-03-22 16:48:45 -0400

    update to listing

--- Frontend/src/components/InternshipListing.vue
+++ Frontend/src/components/InternshipListing.vue
@@ -1,12 +1,34 @@
 <template>
     <div>
-      <h2>Sup</h2>
+        <h2>Sup</h2>
+        <table>
+            <tr v-for="internship in internshipList">
+                <td>{{ internship.id }}</td>
+            </tr>
+        </table>
     </div>
 </template>
 
 <script>
     export default {
-        name: "InternshipListing"
+        name: "InternshipListing",
+        data() {
+            return {
+                internshipList: [],
+                error: false
+            }
+        },
+        created: function () {
+            // Initializing people from backend
+            this.$http.get(`/api/internships`)
+                .then(response => {
+                    // JSON responses are automatically parsed.
+                    this.internshipList = response.data
+                })
+                .catch(e => {
+                    this.error = e;
+                });
+        },
     }
 </script>
 
